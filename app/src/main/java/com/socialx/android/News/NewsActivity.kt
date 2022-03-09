eb79ac922bd441657da3420ace856cfb1784629c
Pratik Chakraborty <chakrabortypratik2000@gmail.com>
2022-03-10 02:08:07 +0530

    auth-complete and recycler view ui complete

---
+++ app/src/main/java/com/socialx/android/News/NewsActivity.kt
@@ -0,0 +1,12 @@
+package com.socialx.android.News
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.socialx.android.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
--- app/src/main/java/com/socialx/android/auth/LoginActivity.kt
+++ app/src/main/java/com/socialx/android/auth/LoginActivity.kt
@@ -1,15 +1,23 @@
 package com.socialx.android.auth
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.socialx.android.MainActivity
+import com.socialx.android.News.NewsActivity
 import com.socialx.android.R
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -39,6 +47,41 @@ open class LoginActivity : BaseActivity(), View.OnClickListener {
         tv_forgotPassword.setOnClickListener(this)
         login_btn.setOnClickListener(this)
         tv_register_user.setOnClickListener(this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            updateUI()
+//            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+//            finish()
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            //updateUI(null)
+            //startActivity(Intent(this, ForgotPasswordActivity::class.java))
+            //finish()
+        }
+    }
+
+    private fun updateUI() {
+        startActivity(Intent(this, NewsActivity::class.java))
+        finish()
     }
 
     override fun onClick(view: View?) {
