c0ab6dc1458bf1af9a521e321552f1ee0d2dbf67
Pratik Chakraborty <chakrabortypratik2000@gmail.com>
2022-03-12 16:21:55 +0530

    reset password activity updated

--- app/src/main/java/com/socialx/android/auth/ForgotPasswordActivity.kt
+++ app/src/main/java/com/socialx/android/auth/ForgotPasswordActivity.kt
@@ -1,7 +1,12 @@
 package com.socialx.android.auth
 
+import android.content.Intent
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
@@ -19,7 +24,7 @@ class ForgotPasswordActivity : BaseActivity() {
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.statusBarColor = this.resources.getColor(R.color.red)
         supportActionBar?.hide()
-
+        emailSentSuccessText.visibility = View.GONE
 
         forgotPasswordSubmit.setOnClickListener {
             val email: String = et_email.text.toString().trim { it <= ' ' }
@@ -31,11 +36,17 @@ class ForgotPasswordActivity : BaseActivity() {
                     .addOnCompleteListener { task ->
                         hideProgressDialog()
                         if(task.isSuccessful) {
-                            Toast.makeText(this@ForgotPasswordActivity,
+                            /*Toast.makeText(this@ForgotPasswordActivity,
                                 resources.getString(R.string.email_sent_success),
                                 Toast.LENGTH_LONG
-                            ).show()
-                            finish()
+                            ).show()*/
+                                emailSentSuccessText.visibility = View.VISIBLE
+                            //finish()
+                            Handler(Looper.myLooper()!!).postDelayed({
+                                val intent = Intent(this, LoginActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }, 4000)
                         } else {
                             showErrorFunction(task.exception!!.message.toString(), true)
                         }
